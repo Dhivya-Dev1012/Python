2449d5162cc99f80a62efde08c9c8b2ff77eb7ac
Dhivya-Dev1012 <sdhivya.neyveli@gmail.com>
2025-05-25 23:46:44 +0530

    Delete OddEven.py

--- OddEven.py
+++
@@ -1,8 +0,0 @@
-class OddEven:
-    def OddEven():
-        num=int(input("Enter a number: "))
-        if(num%2==0):
-            message=f"{num} is Even Number"
-        else:
-            message=f"{num} is Odd Number"
-        return message
